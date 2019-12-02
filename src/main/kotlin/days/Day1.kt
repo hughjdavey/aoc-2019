ec250d9ef134787dd4774485d6725a58fece9b7e
hughjdavey <hughjd@googlemail.com>
2019-12-02 18:50:54 +0000

    Day 1 📆
    Add input file and solutions for both parts of day 1

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,15 +3,26 @@ package days
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        return inputList.map { Module(it.toInt()) }
+                .map { it.fuelRequired() }.sum()
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.toUpperCase() }
-            .last()
+        return inputList.map { Module(it.toInt()) }
+                .map { it.fuelRequiredIncludingSelf() }.sum()
+    }
+
+    data class Module(val mass: Int) {
+
+        fun fuelRequired(): Int {
+            return (mass / 3) - 2
+        }
+
+        fun fuelRequiredIncludingSelf(prev: Module = this): Int {
+            val prevFuel = prev.fuelRequired()
+            return if (prevFuel <= 0) 0 else {
+                fuelRequiredIncludingSelf(Module(prevFuel)) + prevFuel
+            }
+        }
     }
 }
---
+++ src/test/kotlin/Day1Test.kt
@@ -0,0 +1,22 @@
+import days.Day1
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.Test
+
+class Day1Test {
+
+    @Test
+    fun testFuelRequired() {
+        assertThat(Day1.Module(12).fuelRequired(), `is`(2))
+        assertThat(Day1.Module(14).fuelRequired(), `is`(2))
+        assertThat(Day1.Module(1969).fuelRequired(), `is`(654))
+        assertThat(Day1.Module(100756).fuelRequired(), `is`(33583))
+    }
+
+    @Test
+    fun testFuelRequiredInlcudingSelf() {
+        assertThat(Day1.Module(14).fuelRequiredIncludingSelf(), `is`(2))
+        assertThat(Day1.Module(1969).fuelRequiredIncludingSelf(), `is`(966))
+        assertThat(Day1.Module(100756).fuelRequiredIncludingSelf(), `is`(50346))
+    }
+}
